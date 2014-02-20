6a05a58545f48d70ee8eabb574059529600f8e7e
priyank6567 <priyank6567@gmail.com>
2014-02-21 01:24:32 +0530

    Updated Lab Architecture

--- Scripts/1config.sh
+++ Scripts/1config.sh
@@ -1,15 +1,9 @@
-
-<!-- saved from url=(0040)ftp://192.168.0.1/pub/scripts/1config.sh -->
-<html><head><meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"></head><body><pre style="word-wrap: break-word; white-space: pre-wrap;">#! /bin/bash
-
-# Configure Yum Repositories
+#Configure Yum Repositories
 cd /etc/yum.repos.d
 rm * -f
 wget ftp://192.168.0.1/pub/configFiles/mainRepo.repo
 wget ftp://192.168.0.1/pub/configFiles/myRepo.repo
 
 #Set DHCP IP
+#Set Cronjobs for update.sh - > download & execute
 #Set Hostname
-
-
-</pre></body></html>
